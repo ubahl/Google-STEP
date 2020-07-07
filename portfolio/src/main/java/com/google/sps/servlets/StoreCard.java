fb8712f39a401c822de5187ebaace262fb49d3bd
ubahl <ubahl@scu.edu>
2020-07-07 16:43:45 +0000

    Gets store card information from zipcode

---
+++ portfolio/src/main/java/com/google/sps/servlets/StoreCard.java
@@ -0,0 +1,27 @@
+/* Store Card:
+Holds the information that goes onto the search results cards:
+- Place ID
+- Store Name
+- Image (TODO)
+- Rating (TODO)
+*/
+
+package com.google.sps.servlets;
+
+import java.net.URL;
+import com.google.maps.model.PlacesSearchResult;
+
+public class StoreCard {
+    String placeId ="";
+    String name = "";
+    URL icon;
+    float rating = 0.0f;
+
+    public StoreCard(PlacesSearchResult store)
+    {
+        placeId = store.placeId;
+        name = store.name;
+        icon = store.icon;
+        rating = store.rating;
+    }
+}
